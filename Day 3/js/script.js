eeb3f6a0e9c49b5cd238ef2ba3a91f25cc23f5c9
Keownc <kcreese@fullsail.edu>
2015-01-10 20:17:47 -0500

    This is long long over due
    Set up basic file and started coding.

---
+++ Day 3/js/script.js
@@ -0,0 +1,97 @@
+/*
+Keown Creese
+1/10/2015
+Day 3 Coding
+ */
+
+// Single lined Comment
+
+//This is an alert
+//alert("Testing to see if this is connected!");
+
+//Console.log _ allow programmers to see information
+console.log("This is the console.");
+
+
+//Declare a variable
+//Use the keyword of var
+var whatever1;   //Declaration of a variable
+whatever1 = 42; // Definition of a variable
+
+console.log(whatever1);
+
+//Declare and define a variable
+var a = 2;
+console.log(a);
+
+//simple math
+a = a+3;
+console.log(a);
+
+var b;
+b= a+3
+console.log(b);
+console.log(a);
+
+//Find our age
+var YearOfBirth=1992;
+
+//age is calculated by current year minus year born
+var currentYear=2015;
+
+var age = currentYear - YearOfBirth;
+console.log(age);
+
+//Math
+//+,-,*,/
+
+//find area of triangle
+
+//1/2 * base * height
+var base = 8;
+var height = 10;
+var areaTriangle = 1/2 * base * height;
+console.log(areaTriangle);
+
+//Modulo - %
+//Gives remainder
+
+var remainder = 32%10;
+console.log(remainder);
+
+//Find if even or odd
+// modulo by 2 %2
+//1 if it is odd
+// 0 if even
+var evenOrOdd = 67%2;
+console.log(evenOrOdd);
+
+// Assignment Operators
+/*
+= Assignment
+++ Add1 to the variable
+-- subtracts 1 from the variable
+ += (number) addition Assignment
+-= (number) subtracts Assignments
+/= (number) Dividition assignment
+*= (num) multimat
+ */
+
+var counter = 1 ;
+counter++; // counter = counter + 1;
+console.log(counter);
+
+counter--; //counter = counter - 1;
+console.log(counter);
+
+counter +=3; //counter + 3;
+console.log(counter);
+
+counter -=2; // counter= counter - 2;
+console.log(counter);
+
+counter /= 2; //counter = counter / 2
+console.log(counter);
+
+counter *=4; //counter same as counter *4
+console.log(counter);
