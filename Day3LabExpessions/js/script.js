22150e6a16637e3be5d28f8f095345483e03342d
Keownc <kcreese@fullsail.edu>
2015-01-11 00:06:17 -0500

    First solution
    Complete the first part of the code.

--- Day3LabExpessions/js/script.js
+++ Day3LabExpessions/js/script.js
@@ -2,11 +2,25 @@
 Keown Creese
 1/10/2015
 SDI
+Lab 3
 Section 1
  Instructional Expressions
 */
 
-//
+//calculates how much pizza each partyGoer will get at the party
+var slicesOfPizza = 8;
+var numberOfPeople = 10;
+var pizzaOrdered = 4;
+
+// mutlipity slicesOfPizza * pizzaOrdered
+var numberOfSlices = slicesOfPizza * pizzaOrdered;
+
+//Output number of slices at the party
+console.log("The number of pizza slices at the party is "+ numberOfSlices);
+
+
+
+
 
 
 
