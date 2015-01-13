62bf8df633cfafe2b2a09a5e0fa27618921d74c8
Keownc <kcreese@fullsail.edu>
2015-01-13 18:17:06 -0500

    Basic array
    created an array for the avengers.

--- Day 4/js/day 4.js
+++ Day 4/js/day 4.js
@@ -8,3 +8,39 @@ Arrays
 
 //alert("Test to see if this connecting");
 
+// Create a basic array
+var avengersNames=["Thor","Captain America","Iron Man"];
+
+//print it out to the console
+console.log(avengersNames);
+
+//print ot one specific item
+console.log(avengersNames[1]);
+
+//Switch a team member - Iron man for spider
+avengersNames[2]="Spider-Man";
+
+console.log(avengersNames);
+
+//add a team member
+avengersNames[3]="Black Widow"
+
+//print it out
+console.log(avengersNames);
+
+//use a variable as an index #
+var num=2;
+console.log(avengersNames[num]);
+
+//length property
+//Dot syntax - fancy for use a period
+console.log(avengersNames.length);
+
+//last index # is the length - 1
+
+//next open space? is the length
+
+//recruit a new team member
+avengersNames[avengersNames.length]="Powerman";
+console.log(avengersNames);
+console.log(avengersNames.length);
