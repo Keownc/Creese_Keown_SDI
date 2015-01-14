cb2effc0a030a92a2d1107d624d1408d4af655b6
Keownc <kcreese@fullsail.edu>
2015-01-13 23:54:36 -0500

    Added variables
    created an array and a prompt to the file to interact with the user.

--- Creese_Keown_Expression_Assignment/js/Creese_Keown_Expression_Assignments.js
+++ Creese_Keown_Expression_Assignment/js/Creese_Keown_Expression_Assignments.js
@@ -8,3 +8,15 @@ Expression Assignment
 
 //alert("Testing to see if this is working")
 
+
+ //Created an array for groceries in a basket
+ var groceries = ["Milk", " Sugar", "and Bread"];
+
+ // Add a prompt to ask the user to enter their given value
+ var milk = parseInt(prompt("To calculate the average cost sent on groceries.\n What is the price of a jug of milk"));
+ var sugar = parseInt(prompt("To calculate the average cost sent on groceries.\n What is the price of a lb of sugar"));
+ var bread = parseInt(prompt("To calculate the average cost sent on groceries.\n What is the price of a bag of bread"));
+
+
+
+
