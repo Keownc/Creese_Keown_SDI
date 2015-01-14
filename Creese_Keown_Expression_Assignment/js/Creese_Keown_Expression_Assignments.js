16b17b5962fd5b21d1604ef002ee256a6b8824b0
Keownc <kcreese@fullsail.edu>
2015-01-14 12:29:09 -0500

    Changes
    I reworded the my problem and added a calculator so that the user can
    enter and find the sum.

--- Creese_Keown_Expression_Assignment/js/Creese_Keown_Expression_Assignments.js
+++ Creese_Keown_Expression_Assignment/js/Creese_Keown_Expression_Assignments.js
@@ -10,13 +10,26 @@ Expression Assignment
 
 
  //Created an array for groceries in a basket
- var groceries = ["Milk", " Sugar", "and Bread"];
+ var gear = ["iphone6", " memory card", "and iphone case"];
 
  // Add a prompt to ask the user to enter their given value
- var milk = parseInt(prompt("To calculate the average cost sent on groceries.\n What is the price of a jug of milk"));
- var sugar = parseInt(prompt("To calculate the average cost sent on groceries.\n What is the price of a lb of sugar"));
- var bread = parseInt(prompt("To calculate the average cost sent on groceries.\n What is the price of a bag of bread"));
+ var iphone6 = parseInt(prompt("To calculate the cost of a half priced gear.\n What is the price of a iphone6.\n Please do not add dollar sign."));
+ var memoryCard = parseInt(prompt("To calculate the cost of a half priced gear\n What is the price of a Memory Card.\nPlease do not add dollar sign."));
+ var iphoneCase = parseInt(prompt("To calculate the cost of a half priced gear.\n What is the price of a iphone case.\nPlease do not add dollar sign."));
 
+
+ //Created the the average cost
+ // iphone is given at half price
+ // Tax is to be added
+ var cost2= iphone6 /=2;
+ var totalCost= cost2 + memoryCard + iphoneCase;
+ var totalFinCost = totalCost + (totalCost*=15/100);
+
+ //Rounded off to two decimal places
+ totalAvgCost = totalFinCost.toFixed(2);
+
+ //Print the cost
+ console.log("The total amount spent on "+gear+ " is $"+totalFinCost+" after the iphone6 was given at half price \n and 15% taxes has been add to the bill.");
 
 
 
