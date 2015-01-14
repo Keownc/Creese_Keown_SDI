dc39f9294fc85677516d1119a40510e414321f7b
Keownc <kcreese@fullsail.edu>
2015-01-14 13:37:59 -0500

    Results
    Added by results after testing to see if the calculator worked.

--- Creese_Keown_Expression_Assignment/js/Creese_Keown_Expression_Assignments.js
+++ Creese_Keown_Expression_Assignment/js/Creese_Keown_Expression_Assignments.js
@@ -13,16 +13,15 @@ Expression Assignment
  var gear = ["iphone6", " memory card", "and iphone case"];
 
  // Add a prompt to ask the user to enter their given value
- var iphone6 = parseInt(prompt("To calculate the cost of a half priced gear.\n What is the price of a iphone6.\n Please do not add dollar sign."));
- var memoryCard = parseInt(prompt("To calculate the cost of a half priced gear\n What is the price of a Memory Card.\nPlease do not add dollar sign."));
- var iphoneCase = parseInt(prompt("To calculate the cost of a half priced gear.\n What is the price of a iphone case.\nPlease do not add dollar sign."));
+ var iphone6 = parseInt(prompt("To calculate the cost spent on a half-priced gear.\n What is the price of a iphone6.\n Please do not add dollar sign."));
+ var memoryCard = parseInt(prompt("To calculate the cost spent on a half-priced gear\n What is the price of a Memory Card.\nPlease do not add dollar sign."));
+ var iphoneCase = parseInt(prompt("To calculate the cost spent on a half-priced gear.\n What is the price of a iphone case.\nPlease do not add dollar sign."));
 
-
- //Created the the average cost
- // iphone is given at half price
- // Tax is to be added
- var cost2= iphone6 /=2;
- var totalCost= cost2 + memoryCard + iphoneCase;
+//Iphone is given at half price.
+ iphone6 /=2;
+// Add three items to find the cost before tax
+ var totalCost= iphone6 += memoryCard += iphoneCase;
+ // Total cost after tax is to be added
  var totalFinCost = totalCost + (totalCost*=15/100);
 
  //Rounded off to two decimal places
@@ -31,5 +30,8 @@ Expression Assignment
  //Print the cost
  console.log("The total amount spent on "+gear+ " is $"+totalFinCost+" after the iphone6 was given at half price \n and 15% taxes has been add to the bill.");
 
-
-
+//I enter 200 for the iphone6, 60 for the memory card, 35 for the iphone cast and received an answer of $224.25.;
+ //I enter 320 for the iphone6,  120 for the memory card, 60 for the iphone cast and received an answer of $391.;
+ //I enter 400 for the iphone6, 75 for the memory card, 55 for the iphone cast and received an answer of $379.50.;
+ //I enter 240 for the iphone6, 55 for the memory card, 82 for the iphone cast and received an answer of $295.55.;
+ //I enter 284 for the iphone6, 100 for the memory card, 75 for the iphone cast and received an answer of $364.55.;
