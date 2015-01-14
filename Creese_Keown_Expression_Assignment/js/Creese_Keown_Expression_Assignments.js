3ae73a02c01906904a5c5da1165605b038471fd8
Keownc <kcreese@fullsail.edu>
2015-01-14 18:44:08 -0500

    Review
    Made correction to the js file due to the error on my Github account.

--- Creese_Keown_Expression_Assignment/js/Creese_Keown_Expression_Assignments.js
+++ Creese_Keown_Expression_Assignment/js/Creese_Keown_Expression_Assignments.js
@@ -9,20 +9,20 @@ Expression Assignment
 //alert("Testing to see if this is working")
 
 
- //Created an array for groceries in a basket
- var gear = ["Milk", " Sugar", "and Bread"];
+ //Created an array for gear bought at a store
+ var gear = ["iphone6", " Memory Card", "and iphone Case"];
 
  // Add a prompt to ask the user to enter their given value
- var iphone6 = parseInt(prompt("To calculate the cost of a half priced gear.\n What is the price of a iphone6.\n Please do not add dollar sign."));
- var memoryCard = parseInt(prompt("To calculate the cost of a half priced gear\n What is the price of a Memory Card.\nPlease do not add dollar sign."));
- var iphoneCase = parseInt(prompt("To calculate the cost of a half priced gear.\n What is the price of a iphone case.\nPlease do not add dollar sign."));
+ var iphone6 = parseInt(prompt("To calculate the cost of a half priced gear.\nWhat is the price of a iphone6.\nPlease do not add dollar sign."));
+ var memoryCard = parseInt(prompt("To calculate the cost of a half priced gear\nWhat is the price of a Memory Card.\nPlease do not add dollar sign."));
+ var iphoneCase = parseInt(prompt("To calculate the cost of a half priced gear.\nWhat is the price of a iphone case.\nPlease do not add dollar sign."));
 
 
  //Created the the average cost
  // iphone is given at half price
  // Tax is to be added
- var cost2= iphone6 /=2;
- var totalCost= cost2 + memoryCard + iphoneCase;
+ iphone6 /=2;
+ var totalCost= iphone6 += memoryCard += iphoneCase;
  var totalFinCost = totalCost + (totalCost*=15/100);
 
  //Rounded off to two decimal places
