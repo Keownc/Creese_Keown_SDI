954bdda01ff0eeaf32e3f663196a78803ec1bfec
Keownc <kcreese@fullsail.edu>
2015-01-14 22:51:04 -0500

    Spelling
    Checked spelling.

--- Creese_Keown_Expression_Assignment/js/Creese_Keown_Expression_Assignments.js
+++ Creese_Keown_Expression_Assignment/js/Creese_Keown_Expression_Assignments.js
@@ -31,8 +31,8 @@ Expression Assignment
  //Print the cost
  console.log("The total amount spent on "+gear+ " is $"+totalFinCost+" after the iphone6 was given at half price \n and 15% taxes has been add to the bill.");
 
- //I enter 200 for the iphone6, 60 for the memory card, 35 for the iphone cast and received an answer of $224.25.;
- //I enter 320 for the iphone6,  120 for the memory card, 60 for the iphone cast and received an answer of $391.;
- //I enter 400 for the iphone6, 75 for the memory card, 55 for the iphone cast and received an answer of $379.50.;
- //I enter 240 for the iphone6, 55 for the memory card, 82 for the iphone cast and received an answer of $295.55.;
- //I enter 284 for the iphone6, 100 for the memory card, 75 for the iphone cast and received an answer of $364.55.; 
+ //I entered 200 for the iphone6, 60 for the memory card, 35 for the iphone cast and received an answer of $224.25.;
+ //I entered 320 for the iphone6, 120 for the memory card, 60 for the iphone cast and received an answer of $391.;
+ //I entered 400 for the iphone6, 75 for the memory card, 55 for the iphone cast and received an answer of $379.50.;
+ //I entered 240 for the iphone6, 55 for the memory card, 82 for the iphone cast and received an answer of $295.55.;
+ //I entered 284 for the iphone6, 100 for the memory card, 75 for the iphone cast and received an answer of $364.55.;
