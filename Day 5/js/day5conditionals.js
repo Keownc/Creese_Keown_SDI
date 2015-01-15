823508027d0e27512cf9a0c6795d642e2add8996
Keownc <kcreese@fullsail.edu>
2015-01-15 18:55:54 -0500

    Basic if Statement
    Created and tested an if statement.

--- Day 5/js/day5conditionals.js
+++ Day 5/js/day5conditionals.js
@@ -7,3 +7,27 @@ Conditionals
  */
 
 //alert("Testing to see if this is working")
+
+
+ //Basic Conditional Statement
+
+ // Create a boolean variable to test
+ var oldEnough = true;
+
+ //If the child is old enough, then print to the console, You can ride.
+
+ /*
+ Basic Structure of if statement
+
+ if(condition to test){
+ Code to run if the condition is true
+ }
+  */
+
+ if(oldEnough){
+  //code inside of {} will run if the test is true
+  //This code will be skipped if the test is false
+  console.log("you can ride the coaster");
+ }
+
+ console.log("What comes after the if statement.");
