a6ffd169a6300b77eaced0e92f1d85126a957a91
Keownc <kcreese@fullsail.edu>
2015-01-15 20:53:03 -0500

    Learned logial
    Learn how to use logic coding.

--- Day 5/js/day5logicaloperators.js
+++ Day 5/js/day5logicaloperators.js
@@ -38,3 +38,27 @@ Logic Operators
   console.log("Do not buy the 3DS! Because you are broken!");
 
  }
+
+
+ //let buy a smart tv if it is lees than OR we won the lottery
+
+ var tvPrice = 540;
+ var tvBudget = 500;
+ var wonLottery = false;
+ if(tvPrice<tvBudget || wonLottery){
+  console.log("Let's go buy a tv!");
+
+ }else{
+  console.log("Stick with your small tv!")
+ }
+
+
+
+
+
+
+
+
+
+
+
