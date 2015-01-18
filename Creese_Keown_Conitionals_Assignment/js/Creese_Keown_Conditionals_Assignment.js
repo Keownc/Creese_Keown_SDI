988ec8b755097dce6af928423f75430d301fcd45
Keownc <kcreese@fullsail.edu>
2015-01-18 01:17:05 -0500

    Planning
    Made notes/plan on what I want in my problem.
    Started an if, else if, else statement.

--- Creese_Keown_Conitionals_Assignment/js/Creese_Keown_Conditionals_Assignment.js
+++ Creese_Keown_Conitionals_Assignment/js/Creese_Keown_Conditionals_Assignment.js
@@ -7,4 +7,68 @@
  */
 
 
-alert("Test to see if this connected.");
+//alert("Test to see if this connected.");
+
+//Traveling
+
+//age to travel 19 and old
+//Their is a 10% discount for people between the ages of 35 and older
+
+//How much are you willing to spent.
+//Your location is
+
+//miami=200-449
+//New York>=450
+
+//How much people is traveling?
+//2 <=50
+//5>=75
+
+
+//totalbill=cost1+ person
+
+//cost1=250
+//discount= cost*=10
+//totalDiscount=discount/=100
+//change=check-cost1
+
+//cost2=450
+//discount2= cost2*=10
+//totalDiscount= discount2/=100
+//change=check-cost2
+
+
+var age=parseInt(prompt("What is your age?"));
+var check;
+var persons;
+
+if (age>=19) {
+    check=parseInt(prompt("How much are you willing to spent?"));
+    
+    
+} else if (age>=1) {
+    console.log("You are not old enough to pass this point.")
+} else {
+      console.log("You did not enter your age.")
+}
+
+   
+
+// console.log("Your final destination is Maimi. The cost for this trip is $250 and your change is $"+total1);
+//console.log("You do not have enough fund for this trip!");
+//parseInt(prompt("Your destination is New york!\nEnter your check amount!"));
+
+    
+
+
+
+
+
+
+
+//var age=parent(prompt("To Find a destination.\nHow much are you willing to spent","400"));
+//var check;
+
+//var total1= check-
+
+
