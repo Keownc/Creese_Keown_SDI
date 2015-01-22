d7d09d3e975825889bc82d07a45ca6d25a95f326
Keownc <kcreese@fullsail.edu>
2015-01-22 17:21:19 -0500

    Validating take 2
    Created basic file in Komodo.

--- Creese_Keown_Loops_Worksheet/js/Creese_Keown_Loops_Worksheet2.js
+++ Creese_Keown_Loops_Worksheet/js/Creese_Keown_Loops_Worksheet2.js
@@ -11,32 +11,35 @@
 
 var persons=prompt("How many person is in the class?");
 
+ 
+    while(isNaN(persons)||persons==="") {
+        
+    persons=prompt("Please enter the number of persons in the class?");
+    
+   var assignments=prompt("How long do it takes for a person to finish an assignment?");
+   
+   while(isNaN(assignments)||assignments==="");{
+    
+    assignments=prompt("Please enter how long it takes a person to complete an assignment?");
+    
+      var time=prompt("What is the total amount of time set for the assignment?");
+      
+      while (isNaN(time)||time==="") {
+        time=prompt("Please enter the total amount of time set for this assignment?");
+      }
+   }
+    }
+    
+
 
     
     
 for (i=persons; i<=time; i++) {
-    
-    if (isNaN(persons)||persons==="") {
-        
-    persons=prompt("Please enter the number of persons in the class?");
    
-    }else{
-        var assignments=prompt("How long do it takes for a person to finish an assignment?");
-
-    }if (isNaN(assignments)||assignments==="") {
-        
-        assignments=prompt("Please enter how long it takes a person to complete an assignment?");
-    
-    }else{
-        var time=prompt("What is the total amount of time set for the assignment?");
-
-    }if (isNaN(time)||time==="") {
-        time=prompt("Please enter the total amount of time set for this assignment?");
-    }
-  persons-=assignments;
+      persons-=assignments;
       time-=assignments;
       
-      console.log("The total number of persons remaindering in the class at the class at the end of "+time+"is"+persons);    
+      console.log("The total number of persons remaindering in the class at the class at the end of "+time+"is"+persons);
 }
 
         
---
+++ Day 8/js/validated.js
@@ -0,0 +1,8 @@
+/*
+ *Keown Creese
+ *SDI
+ *1/22/15
+ *Validating Prompts take2
+ */
+
+alert("Testing")
