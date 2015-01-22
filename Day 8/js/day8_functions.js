f7d0b43e5b4f6fdba1a3d5af3e06005b23021e82
Keownc <kcreese@fullsail.edu>
2015-01-22 18:50:03 -0500

    Basic Functions
    learned about functions.

--- Day 8/js/day8_functions.js
+++ Day 8/js/day8_functions.js
@@ -1,9 +1,91 @@
 /*
  *Keown Creese
  *SDI
- *1/20/15
- *Loops worksheet
- *Functions
+ *1/22/15
+ *Basic Functions
  */
 
-//alert("testing")
+//alert("testing");
+
+
+//functions functionsName(){code to run}
+//function call- "go botton"-functionsName();
+
+// Creating a function that log on to console "hello"
+
+function printHello(){
+    console.log("Hello! You ARE inside of the print Hello function");
+    
+}
+
+//Function call to start our printHello function
+printHello();
+
+printHello();
+printMore();
+
+
+//create a function to print more test
+function printMore() {
+    console.log("Inside of the print more. Here is more text");
+}
+
+
+printHello();
+
+
+//create a funstion the calculates the area of a rectriangle
+
+function calcArea() {
+    //create variables for the width, height and area
+    var width =20
+    var height=30
+    var area= width*height
+    console.log("The area is "+area)
+}
+
+calcArea();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
