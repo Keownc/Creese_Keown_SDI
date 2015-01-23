bc2bf5002711eb6cc78c1647ed5aaf3724bc72e5
Keownc <kcreese@fullsail.edu>
2015-01-22 20:04:19 -0500

    Finish Arg & Par and Started Return
    Finish Arg and par.
    Created basic file for return values.

---
+++ Day 8/js/day8_return.js
@@ -0,0 +1,8 @@
+/*
+ *Keown Creese
+ *SDI
+ *1/22/15
+ *Return
+ */
+
+//alert("Testing")
--- Day 8/js/day_Agr_Par.js
+++ Day 8/js/day_Agr_Par.js
@@ -11,15 +11,39 @@
 //Argurments - goes in the function call
 //Paramters - goes in the function definition
 
+//Must have the same number of arguments and parameters......usually
+
 //create a function to calculate the area of a retangle
 
-function calcArea() {
+function calcArea(w,h) {//Parameters go here
     
-    var width=10;
-    var height=20;
-    var area=width*height;
+    //var width=10;
+    //var height=20;
+    //var area=width*height;
     
+    var area=w*h;
+    var result=h+w
     console.log("The area is "+area);
+    console.log("The result is "+result);
 }
 
+//call to function
+calcArea(10,20);
 calcArea();
+
+// Calculate Dog years
+//Put in human years, get out dog year
+function dogYears(humanAge) {//humanAge parameter
+    
+    //dog years = human*7
+    var dogAge=humanAge*7;
+    console.log("Dog age is "+dogAge);
+
+}
+
+//Function call with arguments
+
+dogYears(24);
+
+var userAge=prompt("How old are you");
+dogYears(Number(userAge));
