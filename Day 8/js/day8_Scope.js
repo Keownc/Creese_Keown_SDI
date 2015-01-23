7bd9647b1733beff45bc38fbfb04b2055299dc18
Keownc <kcreese@fullsail.edu>
2015-01-22 19:44:30 -0500

    Argument and Parameters
    created basic files.

--- Day 8/js/day8_Scope.js
+++ Day 8/js/day8_Scope.js
@@ -5,4 +5,53 @@
  *scope
  */
 
-//alert("TESTING")
+//alert("TESTING");
+
+
+
+//Variable scope
+//Variable inside and ourside of a function
+
+//try not to use the same variable name
+//However in large file this is going to be impossible
+
+//Variable created inside a function can only be seen inside of those functions
+
+//Created a variable for width in our main code
+
+var width=5; //scope outside of the function or main code
+
+//Create a function the calculates the perimeters or a rectangle
+function calcPeri() {
+    
+    var width=10; //scoped to the function calcPeri
+    console.log("Inside of the function the value of width is "+width);
+    
+    var height=20
+    var perimeter=width*2 + height*2;
+    console.log("Inside of function the perimeter is "+perimeter);
+    
+}
+
+console.log("Before call "+width);
+
+calcPeri();
+
+console.log("After call "+width)
+
+
+//This will not
+//Can not work becuase a variable declared inside of function from your main code.
+console.log("Afte call the perimeter is "+perimeter)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Day 8/js/day_Agr_Par.js
@@ -0,0 +1,25 @@
+/*
+ *Keown Creese
+ *SDI
+ *1/20/15
+ *Arguments and Parameters
+ */
+
+//alert("Testing");
+
+
+//Argurments - goes in the function call
+//Paramters - goes in the function definition
+
+//create a function to calculate the area of a retangle
+
+function calcArea() {
+    
+    var width=10;
+    var height=20;
+    var area=width*height;
+    
+    console.log("The area is "+area);
+}
+
+calcArea();
