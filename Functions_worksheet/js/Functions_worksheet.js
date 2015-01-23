b9fbaa4c93780123156a9b8758bf331913940f76
Keownc <kcreese@fullsail.edu>
2015-01-22 22:37:58 -0500

    Function for bee sting
    created a function for bee sting.

--- Functions_worksheet/js/Functions_worksheet.js
+++ Functions_worksheet/js/Functions_worksheet.js
@@ -33,3 +33,17 @@ while(isNaN(userInput)||userInput===""){
 var results=circleCum(userInput);
 
 console.log("The circumference of the circle is "+results.toFixed(2));
+
+
+
+//Function to calculate how many bee strings kills an animal 
+function beeStung(weight) {
+    
+    //calculate for bee stings
+    var kills=8.666666667*weight
+    
+}
+
+
+
+
