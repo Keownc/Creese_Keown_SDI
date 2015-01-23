9121579caa4b5bf0e9dd481a6b7afb7a3f13a7cf
Keownc <kcreese@fullsail.edu>
2015-01-22 22:24:55 -0500

    Circumference
    Created a function to find the circumference of a circle.
    Prompt of user in input radius.
    Loop the prompt so user will enter a number.

--- Functions_worksheet/js/Functions_worksheet.js
+++ Functions_worksheet/js/Functions_worksheet.js
@@ -5,4 +5,31 @@
  *Functions worksheet
  */
 
-alert("Testing")
+//alert("Testing")
+
+
+// function to calculate circumference of a circle
+function circleCum(radius) {
+    
+    //circumference of circle equals PI*D
+    var diameter=radius*2
+    var circumference=Math.PI*diameter;
+    
+    //Return the circumference
+    return circumference;
+}
+
+//prompt for the using to input the radius
+var userInput=parseInt(prompt("To calculate the circumference of a circle.\n Please enter the radius:"));
+
+//validation to see if the user enter a number
+while(isNaN(userInput)||userInput===""){
+    
+    userInput=parseInt(prompt("Sorry! I only take numbers.\n Please enter the radius:"));
+}
+
+
+//Return to catch the return
+var results=circleCum(userInput);
+
+console.log("The circumference of the circle is "+results.toFixed(2));
