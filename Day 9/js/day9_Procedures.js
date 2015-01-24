92d4fb201b47092c3c1c064e8aa03991891c7cf0
Keownc <kcreese@fullsail.edu>
2015-01-24 18:16:20 -0500

    Finished RandomNumber and procedure
    Finished random numbers.
    started basic file for procedures.

---
+++ Day 9/js/day9_Procedures.js
@@ -0,0 +1,33 @@
+/*
+ *Keown Creese
+ *SDI
+ *1/24/15
+ *Procedures
+ */
+
+//alert("Testing")
+
+//This is a basic or nomal function
+function calcArea(width, height) {
+    var area=width*height
+    return area;
+}
+
+//function call
+var results=calcArea(4,5);
+console.log(result);
+
+//This is a procedure
+//It does not return a value
+//list of steps to do
+
+function calcAreaP(width, height) {
+    
+    //calculation
+    var area=width*height
+    console.log(area);
+    //It will not have a return
+}
+
+//function call for procedure
+calcAreaP(6,7);
