c9ed51530d668b3729309d6ee690868d77e8c7e5
Keownc <kcreese@fullsail.edu>
2015-01-24 22:22:59 -0500

    Function assignment
    Created basic file for function assignments.
    Created a Normal function.
    created an anonymous function.

---
+++ Creese_Keown_Functions_Assignment/js/Creese_Keown_Functions_Assignment.js
@@ -0,0 +1,35 @@
+/*
+ *Keown Creese
+ *SDI
+ *1/24/15
+ *Functions Assignment
+ */
+
+//alert("Testing")
+
+
+//To calculate the cost spent to go to a theme park
+
+function themePark(g,t,s) {
+
+    //calculator
+    var total=g+t+s;
+    //return total
+    return total;
+
+}
+
+// Anonymous Function to calculate addition cost
+var extras=function (m,s){
+        
+        //calculator for additional cost spent at the theme park
+        var total=m+s;
+        
+        //return  for both total and total2;
+        return total;
+    
+};
+
+
+
+
