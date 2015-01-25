9ed29cbdcda92601dc8b4958183525ae438307a6
Keownc <kcreese@fullsail.edu>
2015-01-24 22:24:36 -0500

    While loop
    Created three prompts for the first function.
    Also created a validation for each prompt.

--- Creese_Keown_Functions_Assignment/js/Creese_Keown_Functions_Assignment.js
+++ Creese_Keown_Functions_Assignment/js/Creese_Keown_Functions_Assignment.js
@@ -31,5 +31,49 @@ var extras=function (m,s){
 };
 
 
+//prompt for the cost spent on gas
+var gas=prompt("The calculate the cost to go to a theme park.\nHow much money was spent on gas:");
+
+//validation for for gas money
+while (isNaN(gas)||gas==="") {
+    
+    if (isNaN(gas)) {
+        gas=prompt("Please only enter numbers.\nHow much money was spent on gas:");
+    }else{
+        age=prompt("Please do not leave blank.\nHow much money was spent on gas:");
+    }
+}
+
+//Prompt for the cost of ticket
+ var ticket=prompt("What is the cost for the ticket:");
+
+//validation for ticket
+while (ticket===""||isNaN(ticket)) {
+    
+    //An if else statement to see if the user left blank or enter words
+    if (isNaN(ticket)) {
+    ticket=prompt("Please only enter numbers.\nWhat is the cost of the ticket:");
+    
+    }else{
+        ticket=prompt("Please do not leave blank.\nWhat is the cost of the ticket:");
+    }
+}
+
+//Prompt for snack bought at the theme park
+var snacks=prompt("How money did you spent on snacks");
+
+//valiation for snacks to see if a number was entered
+while (isNaN(snacks)||snacks===""){
+    
+    //An if else statement to see if the user left blank or enter words
+    if (isNaN(snacks)) {
+    snacks=prompt("Please only enter numbers.\nWhat is the cost of the ticket:");
+    
+    }else{
+        snacks=prompt("Please do not leave blank.\nWhat is the cost of the ticket:");
+    }
+}
+
+
 
 
