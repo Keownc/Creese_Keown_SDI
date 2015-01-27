9460cb202ef5a307b22b89f67bdb9e475bcc2cf8
Keownc <kcreese@fullsail.edu>
2015-01-26 19:45:08 -0500

    ParseInt
    ParseInt the prompt so that the code would not see the number entered
    as a text string.

--- Creese_Keown_Functions_Assignment/js/Creese_Keown_Functions_Assignment.js
+++ Creese_Keown_Functions_Assignment/js/Creese_Keown_Functions_Assignment.js
@@ -10,20 +10,20 @@
 
 //To calculate the cost spent to go to a theme park
 
-function themePark(gas,ticket,snacks) {
+function themePark(g,t,s) {
 
     //calculator
-    var total=gas+ticket+snacks;
+    var total= g + t + s;
     //return total
     return total;
 
 }
 
 // Anonymous Function to calculate addition cost
-var extras=function (movie,souvenir){
+var extras=function (m,s){
         
         //calculator for additional cost spent at the theme park
-        var total=movie+souvenir;
+        var total=m+s;
         
         //return  for both total and total2;
         return total;
@@ -32,73 +32,73 @@ var extras=function (movie,souvenir){
 
 
 //prompt for the cost spent on gas
-var gas=prompt("The calculate the cost to go to a theme park.\nHow much money was spent on gas:");
+var gas=parseInt(prompt("The calculate the cost to go to a theme park.\nHow much money was spent on gas:"));
 
 //validation for for gas money
 while (isNaN(gas)||gas==="") {
     
     if (isNaN(gas)) {
-        gas=prompt("Please only enter numbers.\nHow much money was spent on gas:");
+        gas=parseInt(prompt("Please only enter numbers.\nHow much money was spent on gas:"));
     }else{
-        age=prompt("Please do not leave blank.\nHow much money was spent on gas:");
+        age=parseInt(prompt("Please do not leave blank.\nHow much money was spent on gas:"));
     }
 }
 
 //Prompt for the cost of ticket
- var ticket=prompt("What is the cost for the ticket:");
+ var ticket=parseInt(prompt("What is the cost for the ticket:"));
 
 //validation for ticket
 while (ticket===""||isNaN(ticket)) {
     
     //An if else statement to see if the user left blank or enter words
     if (isNaN(ticket)) {
-    ticket=prompt("Please only enter numbers.\nWhat is the cost of the ticket:");
+    ticket=parseInt(prompt("Please only enter numbers.\nWhat is the cost of the ticket:"));
     
     }else{
-        ticket=prompt("Please do not leave blank.\nWhat is the cost of the ticket:");
+        ticket=parseInt(prompt("Please do not leave blank.\nWhat is the cost of the ticket:"));
     }
 }
 
 //Prompt for snack bought at the theme park
-var snacks=prompt("How money did you spent on snacks");
+var snacks=parseInt(prompt("How money did you spent on snacks"));
 
 //valiation for snacks to see if a number was entered
 while (isNaN(snacks)||snacks===""){
     
     //An if else statement to see if the user left blank or enter words
     if (isNaN(snacks)) {
-    snacks=prompt("Please only enter numbers.\nWhat is the cost of the ticket:");
+    snacks=parseInt(prompt("Please only enter numbers.\nWhat is the cost of the ticket:"));
     
     }else{
-        snacks=prompt("Please do not leave blank.\nWhat is the cost of the ticket:");
+        snacks=parseInt(prompt("Please do not leave blank.\nWhat is the cost of the ticket:"));
     }
 }
 
 //Prompt for the cost spent to watch in movie in the park
-var movie=prompt("What is the cost spent to watch a movie at the park.");
+var movie=parseInt(prompt("What is the cost spent to watch a movie at the park."));
 
 //validation for cost spent on the movie
 while (isNaN(movie)||movie===""){
     //An if else statement to see if the user left blank or enter words
     if (isNaN(movie)) {
-    movie=prompt("Please only enter numbers.\nWhat is the cost to watch a movie:");
+    movie=parseInt(prompt("Please only enter numbers.\nWhat is the cost to watch a movie:"));
     
     }else{
-        movie=prompt("Please do not leave blank.\nWhat is the cost to watch a movie:");
+        movie=parseInt(prompt("Please do not leave blank.\nWhat is the cost to watch a movie:"));
     }
 }
 
 //Prompt for the cost spent to watch in movie in the park
-var souvenir=prompt("What is the cost spent to watch a movie at the park.");
+var souvenir=parseInt(prompt("What is the cost spent to watch a movie at the park."));
 
 //validation for cost spent on the movie
 while (isNaN(souvenir)||souvenir===""){
     //An if else statement to see if the user left blank or enter words
     if (isNaN(souvenir)) {
-    souvenir=prompt("Please only enter numbers.\How much money was spent on souvenir:");
+    souvenir=parseInt(prompt("Please only enter numbers.\How much money was spent on souvenir:"));
     
     }else{
-        souvenir=prompt("Please do not leave blank.\nHow much money was spent on souvenir:");
+        souvenir=parseInt(prompt("Please do not leave blank.\nHow much money was spent on souvenir:"));
     }
 }
 
@@ -109,7 +109,7 @@ while (isNaN(souvenir)||souvenir===""){
 var results1=themePark(gas,ticket,snacks);
 
 //Function call to anonymous extra
-var results2=themePark(gas,ticket,snacks)+extras(movie,souvenir);
+var results2=extras(movie,souvenir);
 
 //calculator for total cost 
 var grandTotal=results1+results2;
