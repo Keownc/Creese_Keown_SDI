36c7144023f0fe0f3e10d5541de39703b8959af2
Keownc <kcreese@fullsail.edu>
2015-01-27 22:57:08 -0500

    loops and if else statement
    Added while loops and if else statement to the code.

--- day 10/Day 4/js/day4_prompting.js
+++ day 10/Day 4/js/day4_prompting.js
@@ -10,6 +10,15 @@ Prompting
 // Ask user for input - prompt()
 //Save the user's response - by creating a variable
 var userInput = prompt("Enter your year of birth?");
+//validation
+while (userInput===""||isNaN(userInput)) {
+    //if else statement
+    if (userInput==="") {
+     userInput = prompt("Please dont leave blank.\nEnter your year of birth?");   
+    }else{
+    userInput = prompt("Please only enter numbers.\nEnter your year of birth?");   
+    }
+}
 console.log(userInput);
 
 //Calculate the area of a rectangle
@@ -17,7 +26,25 @@ console.log(userInput);
 
 //create 2 variable and prompt the user for length and width
 var length = prompt("Lets calculate the area of a rectangle.\nPlease enter in the length:");
+
+while (length===""||isNaN(length)) {
+    //if else statement
+    if (length==="") {
+     length = prompt("Please dont leave blank.\nWhat is the length?");   
+    }else{
+    length= prompt("Please only enter numbers.\What is the length?");   
+    }
+}
+
 var width = prompt("Lets calculate the area of a rectangle.\nPlease enter in the width");
+while (width===""||isNaN(width)) {
+    //if else statement
+    if (width==="") {
+     width = prompt("Please dont leave blank.\nWhat is the width?");   
+    }else{
+    width= prompt("Please only enter numbers.\What is the width?");   
+    }
+}
 
 // calculate area
 var areaRec = length * width;
